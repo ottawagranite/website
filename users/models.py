331907f5878d9aa5b75c5d1a2b1be9b00752a700
Paul Shannon <github@paulshannon.ca>
2013-07-09 14:01:04 -0400

    more 'user' models

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class PhoneNumber(models.Model):
@@ -14,7 +15,7 @@ class PhoneNumber(models.Model):
 
 class EmailAddress(models.Model):
     type = models.CharField(_('type'), help_text=_('The type of email address'), choices=settings.EMAIL_ADDRESS_TYPE_CHOICES)
-    address = models.EmailField(_('address'), help_text=_('The email address'),)
+    address = models.EmailField(_('address'), help_text=_('The email address'))
 
     class Meta:
         abstract = True
@@ -37,50 +38,50 @@ class Address(models.Model):
 
 
 class Member(models.Model):
-    membership_number = models.IntegerField()
-    membership_year = models.IntegerField()
-    number_of_parking_stickers = models.IntegerField()
+    membership_number = models.IntegerField(_('membership number'))
+    latest_membership = models.IntegerField(_('latest_membership'))
+    earliest_membership = models.IntegerField(_('earliest membership'))
 
-    saluation = models.CharField(max_length=100, choices=settings.SALUTATION_CHOICES)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    gender = models.CharField(max_length=100, choices=settings.GENDER_CHOICES)
+    salutation = models.CharField(_('salutation'), max_length=100, choices=settings.SALUTATION_CHOICES)
+    first_name = models.CharField(_('first name'), max_length=100)
+    last_name = models.CharField(_('last name'), max_length=100)
+    gender = models.CharField(_('gender'), max_length=100, choices=settings.GENDER_CHOICES)
 
-    date_of_birth = models.DateField()
-    started_curling = models.IntegerField()
-    rookie_year = models.IntegerField()
+    date_of_birth = models.DateField(_('date of birth'))
+    started_curling = models.IntegerField(_('started curling'))
+    rookie_year = models.IntegerField(_('rookie year'))
 
-    locker_number = models.IntegerField()
+    locker_number = models.IntegerField(_('locker number'))
+    parking_stickers = models.IntegerField(_('parking stickers'))
 
-    social = models.BooleanField(default=False)
-    honourary = models.BooleanField(default=False)
-    deceased = models.BooleanField(default=False)
+    social = models.BooleanField(_('social'), default=False)
+    honorary = models.BooleanField(_('honorary'), default=False)
+    student = models.BooleanField(_('student'), default=False)
 
-    comments = models.TextField(blank=True)
+    comments = models.TextField(_('comments'), blank=True)
+
+    class Meta:
+        abstract = True
+        verbose_name = _('member')
+        verbose_name_plural = _('members')
 
 
 class MemberAddress(Address):
-    member = models.ForeignKey(Member, related_name='address')
+    publish = models.BooleanField(_('publish'), default=False)
+    member = models.ForeignKey(Member, verbose_name=_(''), related_name='address')
 
 
 class MemberEmailAddress(EmailAddress):
-    member = models.ForeignKey(Member, related_name='email')
+    publish = models.BooleanField(_('publish'), default=False)
+    member = models.ForeignKey(Member, verbose_name=_(''), related_name='email')
 
 
 class MemberPhoneNumber(PhoneNumber):
-    member = models.ForeignKey(Member, related_name='phone_number')
+    publish = models.BooleanField(_('publish'), default=False)
+    member = models.ForeignKey(Member, verbose_name=_(''), related_name='phone_number')
 
 ''' Unknown
-Rating    Text    1
-FIRSTYEARGCC    Text    4 DUP on membership_year?
-BOARDMEM    Long Integer    4
 INSTRUCTOR    Text    10
-pres year    Text    12
-OKBookEmail    Long Integer    4
-OKBOOK    Long Integer    4
-STUDENT    Long Integer    4
-JUNIOR    Long Integer    4
-BANTAM    Long Integer    4
 '''
 
 ''' Mailing List
@@ -99,9 +100,14 @@ SATOPEN    Long Integer    4
 SUNOPEN    Long Integer    4
 MIXED    Long Integer    4
 LTLROCK    Long Integer    4
+JUNIOR    Long Integer    4
+BANTAM    Long Integer    4
 RookiesEve    Long Integer    4
 RookiesDay    Long Integer    4
+'''
 
+''' For Leagues
+Rating    Text    1
 '''
 
 ''' Payments and fees
@@ -123,10 +129,15 @@ OverrideFeeDate    Date / Time    8
 
 
 class EmergencyContact(models.Model):
-    member = models.ForeignKey(Member)
-    name = models.TextField()
-    relationship = models.TextField()
+    member = models.ForeignKey(Member, verbose_name=_('member'), related_name='emergency_contact')
+    name = models.TextField(_('name'))
+    relationship = models.TextField(_('relationship'))
+
+    class Meta:
+        abstract = True
+        verbose_name = _('emergency contact')
+        verbose_name_plural = _('emergency contacts')
 
 
 class EmergencyContactPhoneNumber(PhoneNumber):
-    emergency_contact = models.ForeignKey(EmergencyContact, related_name='phone_number')
+    emergency_contact = models.ForeignKey(EmergencyContact, verbose_name=_('emergency_contact'), related_name='phone_number')
