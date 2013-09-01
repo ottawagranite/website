f0e253b156c3e2153c4adc7f602ea0f8c018db1d
Michael P. Soulier <msoulier@digitaltorque.ca>
2013-09-01 11:10:00 -0400

    Working on the front page.

--- ottawagranite/common.py
+++ ottawagranite/common.py
@@ -10,89 +10,16 @@ def main_menu():
                     "href": "/info/about/"
                 },
                 {
+                    "title": "Leagues",
+                    "href": "/leagues/"
+                },
+                {
                     "title": "About the website",
                     "href": "/info/website/"
                 },
                 {
                     "title": "Contact us",
                     "href": "/info/contact/"
-                }
-            ]
-        },
-        {
-            "title": "League information",
-            "submenus": [
-                {
-                    "title": "General information",
-                    "href": "/leagues/general/"
-                },
-                {
-                    "title": "Looking for curlers",
-                    "href": "/leagues/looking/"
-                },
-                {
-                    "title": "divider",
-                    "href": ""
-                },
-                {
-                    "title": "Day Ladies",
-                    "href": "/leagues/dayladies/"
-                },
-                {
-                    "title": "Day Men",
-                    "href": "/leagues/daymen/"
-                },
-                {
-                    "title": "Day Mixed",
-                    "href": "/leagues/daymixed/"
-                },
-                {
-                    "title": "divider",
-                    "href": ""
-                },
-                {
-                    "title": "Evening Ladies",
-                    "href": "/leagues/eveningladies/"
-                },
-                {
-                    "title": "Evening Men",
-                    "href": "/leagues/eveningmen/"
-                },
-                {
-                    "title": "divider",
-                    "href": ""
-                },
-                {
-                    "title": "Weekend Mixed",
-                    "href": "/leagues/weekendmixed/"
-                },
-                {
-                    "title": "Sunday Open",
-                    "href": "/leagues/sundayopen/"
-                },
-                {
-                    "title": "Monday Open Fixed",
-                    "href": "/leagues/mondayopenfixed/"
-                },
-                {
-                    "title": "Wednesday Open",
-                    "href": "/leagues/wednesdayopen/"
-                },
-                {
-                    "title": "divider",
-                    "href": ""
-                },
-                {
-                    "title": "Little Rocks",
-                    "href": "/leagues/littlerocks/"
-                },
-                {
-                    "title": "Bantam / Juniors",
-                    "href": "/leagues/bantamjuniors/"
-                },
-                {
-                    "title": "Rookies Rock",
-                    "href": "/leagues/rookiesrock/"
                 }
             ]
         },
